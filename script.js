5a0271b37269455d660804128b251286fb58c454
playfairs <root@playfairs.cc>
2026-04-17 05:23:19 -0500

    o

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const aboutTexts = [
-        'get up off your knees'
+        "I'm so fucking tired"
     ];
     
     let lastText = '';
